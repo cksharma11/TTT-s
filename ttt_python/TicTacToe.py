13b334d36e102dbbbfb828d6fc3ee159e73f13ba
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-04-18 12:30:43 +0530

    Added TTT Python

---
+++ ttt_python/TicTacToe.py
@@ -0,0 +1,43 @@
+from src.Board import Board
+from src.Player import Player
+from src.Game import Game
+
+def getValidMoveMove(game):
+    move = int(raw_input(game.getCurrentPlayer().getName() + "'s turn :"))
+    while(game.isPlaceEmpty(move)):
+            print("Place is already occupied!")
+            move =  getValidMoveMove(game)
+    return move
+
+def showSymbols(player1, player2):
+    print(player1 + "'s symbol is X")
+    print(player2 + "'s symbol is O")
+
+def createGame():
+    board = Board()
+    player1Name = raw_input("Enter First Player Name : ")
+    player2Name = raw_input("Enter Second Player Name : ")
+    player1 = Player(player1Name, "X")
+    player2 = Player(player2Name, "O")
+    showSymbols(player1Name, player2Name)
+    game = Game(player1, player2, board)
+    return game
+
+def conductGame(game):
+    while(game.isDraw() != True):
+        game.printBoard()
+        currentPlayerMove = getValidMoveMove(game)
+        game.makeMove(currentPlayerMove)
+        if(game.hasWon()):
+            game.printBoard()
+            print(game.getCurrentPlayer().getName() + " has Won!")
+            return
+        game.updateCurrentPlayer()
+
+    print("Draw !")
+
+def main():
+    game = createGame()
+    conductGame(game)
+
+main()
---
+++ ttt_python/src/Player.py
@@ -0,0 +1,17 @@
+class Player:
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
+        self.moves = []
+
+    def makeMove(self, position):
+        self.moves.append(position)
+
+    def getMoves(self):
+        return self.moves
+
+    def getSymbol(self):
+        return self.symbol
+
+    def getName(self):
+        return self.name
